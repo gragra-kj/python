4fd94987a8e569cba700753bb6d79fa484200768
Grace Muthui <muthuigracia27@gmail.com>
2024-01-19 09:50:22 +0300

    first commit

---
+++ grace.py
@@ -0,0 +1,9 @@
+question=input("What do you want in life? ").strip().title()
+#print("I want " +question + " in life")
+#remove whitespace from string
+#question=question.strip()
+#question=question.title()
+#question=question.strip().title()
+#use of f which tells the compiler this a special string
+#split first name and second
+print(f"I want {question} in life")
