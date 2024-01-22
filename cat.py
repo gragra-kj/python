317a4501d9425f0ace2fe96854723dde29177585
Grace Muthui <muthuigracia27@gmail.com>
2024-01-22 12:59:31 +0300

    for,while loops

---
+++ cat.py
@@ -0,0 +1,29 @@
+#while loop
+# i=5
+# while i!=0:
+#     print("meow")
+#     i=i-1
+# i=1
+# while i<=5:
+#     print("meow")
+#     i=i+1
+# i=0
+# while i<5:
+#     print("meow")
+#     i +=1
+
+#for loop
+# for i in [0,1,2,3]:
+#     print("Meow")
+# for i in range(3):
+#     print("Grace")
+# print("Meow \n" *3, end="")
+while True:
+    n=int(input("Whats the value on n? "))
+    if n>0:
+        break
+for _ in range(n):
+    print("Mine")
+
+
+
