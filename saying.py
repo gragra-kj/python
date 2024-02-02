5eee7b56e26f64bfc159bb6a957a72a97218ae89
Grace Muthui <muthuigracia27@gmail.com>
2024-02-02 13:46:26 +0300

    libraries,random,packages,apis

---
+++ saying.py
@@ -0,0 +1,11 @@
+def main():
+    hello("Grace")
+    love("Love")
+def hello(name):
+    print(f"hello,{name}")
+
+def love(name):
+    print(f"hey,{name}")
+
+if __name__ =="_main_":
+    main()
---
+++ says.py
@@ -0,0 +1,6 @@
+import sys
+from saying import love
+
+if len(sys.argv)==2:
+    love(sys.argv[1])
+
